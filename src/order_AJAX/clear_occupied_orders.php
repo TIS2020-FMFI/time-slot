90332f36ed584725bc23bd6bf7ac13b6f19f07eb
unknown <ondrej.richnak@gmail.com>
2021-01-03 18:43:40 +0100

    oprava scriptov priprava php na time-schediular #68

--- src/order_AJAX/clear_occupied_orders.php
+++ src/order_AJAX/clear_occupied_orders.php
@@ -1,5 +1,18 @@
 <?php
-include('../db.php');
+$host = 'localhost';
+$username = 'timeslot';
+$password = 'ieotmsltieo';
+$dbname = 'timeslot';
+
+
+$mysqli = new mysqli($host,$username,$password,$dbname);
+if ($mysqli->connect_errno) {
+    echo '<strong>NEpodarilo sa pripojiť s databazou</strong>';
+} else {
+    $mysqli->query("SET CHARACTER SET 'utf8'");
+}
+
+
 if (!$mysqli->connect_errno) {
     $sql="UPDATE `time_slot`  SET 
                 ocupide_start_time=DEFAULT ,
